21881eb1819be57e067f3e2a7ff378be6ada2b3d
anikahammedkhan <anikkhan1105@gmail.com>
2022-09-18 22:08:50 +0600

    functionality added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,42 @@
-let map = L.map('map').setView([23.78043407217103, 90.42554856487746], 13);
+document.getElementById('button-addon').addEventListener('click',()=>{
+    const inputField = document.getElementById('inputField');
+    const inputFieldValue = inputField.value;
+    let ipAddress = inputFieldValue;
+    const url = `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_EP3bxLFf61aTLFzAiSrAGZ85rMOsX&ipAddress=${ipAddress}`
+    fetch(url)
+    .then( res => res.json())
+    .then(data => loadData(data));
+
+    const loadData =data =>{
+    const lat = data.location.lat;
+    const lng = data.location.lng;
+    const ip = data.ip;
+    const location = data.location.city;
+    const timeZone = data.location.timezone;
+    const isp = data.as?.name;
+    const ipField = document.getElementById('ipField');
+    ipField.innerText = ip;
+    const locationField = document.getElementById('locationField');
+    locationField.innerText = location;
+    const timeZoneField = document.getElementById('timeZoneField');
+    timeZoneField.innerText = timeZone;
+    const ispField = document.getElementById('ispField');
+    ispField.innerText = isp;
+    let map = L.map('map').setView([lat, lng], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
-L.marker([23.781017848472192, 90.42552304726318]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+L.marker([lat, lng]).addTo(map)
+    .bindPopup(location)
     .openPopup();
+}
+inputField.innerText = '';
+})
+
+
+
+
+
+
+
